92a7ad71032cdca6f3e0bce9fda900c65c7e7cd8
ygfest <stefanosanesteban1018@gmail.com>
2024-12-28 23:34:19 +0800

    Added an authjs as authentication with github provider

--- src/app/components/nav-bar.tsx
+++ src/app/components/nav-bar.tsx
@@ -1,17 +1,60 @@
-'use client'
+import Link from "next/link";
+import { auth, signIn, signOut } from "../auth";
 
-//bg-[#020202a0] 
+async function NavigationBar() {
+  const session = await auth();
 
-import React from 'react'
+  // Server action for handling sign-in
+  async function handleSignIn() {
+    "use server";
+    await signIn("github");
+  }
 
+  // Server action for handling sign-out
+  async function handleSignOut() {
+    "use server";
+    await signOut();
+  }
 
-function NavigationBar() {
   return (
-    <div className='fixed flex top-0 left-0 right-0 h-16 bg-white backdrop-blur-xl'>
-      <div className='flex'>
-        <h4 className='text-xl font-medium'>Learn Cryptography</h4></div>
-    </div>
-  )
+    <nav className="fixed left-1/2 top-4 z-40 w-[90%] max-w-[1200px] -translate-x-1/2 rounded-3xl border-2 border-[#300651] px-4 py-3 backdrop-blur-sm backdrop-saturate-150 bg-[#000000] md:px-8 md:py-4">
+      <header className="flex w-full items-center justify-between">
+        <img src="/st-logo.png" alt="logo" width={120} height={35} />
+        <div className="flex gap-1 md:gap-4 items-center">
+          {session && session.user ? (
+            <>
+              <Link
+                href="/startup"
+                className="font-medium min-w-[60px] text-sm md:min-w-[100px] md:text-base p-2 rounded-lg text-primary hover:bg-zinc-200 flex-nowrap"
+              >
+                Get Started
+              </Link>
+              <Link href={`/user/${session?.id}`}>
+                {session?.user?.name}
+              </Link>
+              <form action={handleSignOut} method="post">
+                <button
+                  type="submit"
+                  className="font-medium min-w-[60px] text-sm md:min-w-[100px] md:text-base p-2 rounded-lg text-primary hover:bg-zinc-200 flex-nowrap"
+                >
+                  Log out
+                </button>
+              </form>
+            </>
+          ) : (
+            <form action={handleSignIn} method="post">
+              <button
+                type="submit"
+                className="hidden md:inline p-2 text-sm md:text-base font-medium text-white bg-secondary hover:bg-zinc-600 rounded-lg min-w-[150px] md:min-w-[120px] flex-nowrap"
+              >
+                Log in
+              </button>
+            </form>
+          )}
+        </div>
+      </header>
+    </nav>
+  );
 }
 
 export default NavigationBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+
 import { FiUser } from "react-icons/fi";
 import { SiVite } from "react-icons/si";
 import { FiHelpCircle } from "react-icons/fi";
@@ -7,33 +7,11 @@ import Link from "next/link"
 
 export default function Home() {
   
-
-
   return (
     <main className="bg-[#000000] font-poppins">
       {/* Hero Section */}
       <section className="relative flex h-[80vh] w-full items-center justify-center min-w-[400px]">
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4C0647,transparent_1px),linear-gradient(to_bottom,#300651,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_70%_60%_at_60%_0%,#000_80%,transparent_110%)]" />
-        <nav className="fixed left-1/2 top-4 z-40 w-[90%] max-w-[1200px] -translate-x-1/2 rounded-3xl border-2 border-[#300651] px-4 py-3 backdrop-blur-sm backdrop-saturate-150 bg-[#000000] md:px-8 md:py-4">
-          <header className="flex w-full items-center justify-between">
-            <img src="/st-logo.png" alt="logo" width={120} height={35} />
-            <div className="flex gap-1 md:gap-4 items-center">
-              <Link href="/sign-in">
-                <button className="font-medium min-w-[60px] text-sm md:min-w-[100px] md:text-base p-2 rounded-lg text-primary hover:bg-zinc-200 flex-nowrap">
-                  Get Started
-                </button>
-              </Link>
-              <Link href="/sign-up">
-                <button className="hidden md:inline p-2 text-sm md:text-base font-medium text-white bg-secondary hover:bg-zinc-600 rounded-lg min-w-[150px] md:min-w-[120px] flex-nowrap">
-                  Try now
-                </button>
-                <button className="md:hidden p-2 text-sm md:text-base font-medium text-white bg-secondary hover:bg-zinc-600 rounded-lg min-w-[120px] md:min-w-[120px] flex-nowrap">
-                  Try now
-                </button>
-              </Link>
-            </div>
-          </header>
-        </nav>
+        
         <div className="z-30 w-[90%] max-w-[1200px] pt-4">
           <h1 className="text-center text-[2.3rem] font-bold leading-tight md:text-[3.2rem] md:leading-none lg:text-[4rem] 2xl:text-[4.5rem]">
             <span className="text-white">
